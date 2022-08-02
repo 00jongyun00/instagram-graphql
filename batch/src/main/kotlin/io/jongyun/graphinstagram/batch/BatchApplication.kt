5caf1998a83c20e4edb0a92ffc0c7815731722da
neimen <neimen.95@kakaostyle.com>
2022-08-02 16:01:56 +0900

    Added setup batch module

---
+++ batch/src/main/kotlin/io/jongyun/graphinstagram/batch/BatchApplication.kt
@@ -0,0 +1,11 @@
+package io.jongyun.graphinstagram.batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BatchApplication
+
+fun main(args: Array<String>) {
+	runApplication<BatchApplication>(*args)
+}

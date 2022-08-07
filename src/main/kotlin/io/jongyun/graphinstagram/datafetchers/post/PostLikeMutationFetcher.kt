6ac0ef2aeff7aa0c54cb15508577c7e5bf6dbc6f
nothingprogram <popawaw@naver.com>
2022-08-07 22:15:41 +0900

    [feature] like cancel to post resolver 추가

--- src/main/kotlin/io/jongyun/graphinstagram/datafetchers/post/PostLikeMutationFetcher.kt
+++ src/main/kotlin/io/jongyun/graphinstagram/datafetchers/post/PostLikeMutationFetcher.kt
@@ -5,6 +5,7 @@ import com.netflix.graphql.dgs.DgsData
 import com.netflix.graphql.dgs.InputArgument
 import io.jongyun.graphinstagram.DgsConstants
 import io.jongyun.graphinstagram.service.post.PostLikesService
+import io.jongyun.graphinstagram.types.LikeCancelPostInput
 import io.jongyun.graphinstagram.types.LikePostInput
 import io.jongyun.graphinstagram.util.getAuthName
 
@@ -16,5 +17,10 @@ class PostLikeMutationFetcher(
     @DgsData(parentType = DgsConstants.Mutation_TYPE, field = DgsConstants.MUTATION.PostLike)
     fun addLike(@InputArgument likePostInput: LikePostInput): Boolean {
         return postLikesService.addLike(getAuthName(), likePostInput)
+    }
+
+    @DgsData(parentType = DgsConstants.Mutation_TYPE, field = DgsConstants.MUTATION.PostLikeCancel)
+    fun likeCancel(@InputArgument likeCancelPostInput: LikeCancelPostInput): Boolean {
+        return postLikesService.likeCancel(getAuthName(), likeCancelPostInput)
     }
 }

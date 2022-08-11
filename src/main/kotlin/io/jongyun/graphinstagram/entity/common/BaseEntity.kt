ecf760eda91f7c8d1242a49df7f490f7b617577b
nothingprogram <popawaw@naver.com>
2022-08-11 18:20:00 +0900

    post comments 의 작성자를 찾을때 N + 1 문제 발생 해결

--- src/main/kotlin/io/jongyun/graphinstagram/entity/common/BaseEntity.kt
+++ src/main/kotlin/io/jongyun/graphinstagram/entity/common/BaseEntity.kt
@@ -1,23 +1,23 @@
 package io.jongyun.graphinstagram.entity.common
 
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedDate
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import java.time.LocalDateTime
+import java.time.OffsetDateTime
 import javax.persistence.Column
 import javax.persistence.EntityListeners
 import javax.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseTimeEntity {
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    var createdAt: LocalDateTime = LocalDateTime.now()
+    var createdAt: OffsetDateTime = OffsetDateTime.now()
         protected set
 
     @LastModifiedDate
     @Column(nullable = false)
-    var updatedAt: LocalDateTime = LocalDateTime.now()
+    var updatedAt: OffsetDateTime = OffsetDateTime.now()
         protected set
 }
--- src/main/kotlin/io/jongyun/graphinstagram/util/MapToGraphql.kt
+++ src/main/kotlin/io/jongyun/graphinstagram/util/MapToGraphql.kt
@@ -3,19 +3,17 @@ package io.jongyun.graphinstagram.util
 import io.jongyun.graphinstagram.entity.member.Member
 import io.jongyun.graphinstagram.entity.post.Post
 import io.jongyun.graphinstagram.entity.post.PostComment
-import java.time.ZoneOffset
 import io.jongyun.graphinstagram.types.Member as TypesMember
 import io.jongyun.graphinstagram.types.Post as TypesPost
 import io.jongyun.graphinstagram.types.PostComment as TypesPostComment
 
-val ZONE_OFFSET = ZoneOffset.ofHours(9)
 fun mapToGraphql(post: Post): TypesPost {
     return TypesPost(
         id = post.id.toString(),
         createdBy = mapToGraphql(post.createdBy),
         content = post.content,
-        createdAt = post.createdAt.atOffset(ZONE_OFFSET),
-        updatedAt = post.updatedAt.atOffset(ZONE_OFFSET),
+        createdAt = post.createdAt,
+        updatedAt = post.updatedAt,
     )
 }
 
@@ -23,8 +21,8 @@ fun mapToGraphql(member: Member): TypesMember {
     return TypesMember(
         id = member.id.toString(),
         name = member.name,
-        createdAt = member.createdAt.atOffset(ZONE_OFFSET),
-        updatedAt = member.updatedAt.atOffset(ZONE_OFFSET)
+        createdAt = member.createdAt,
+        updatedAt = member.updatedAt,
     )
 }
 
@@ -33,8 +31,8 @@ fun mapToGraphql(postComment: PostComment): TypesPostComment {
         id = postComment.id.toString(),
         createdBy = mapToGraphql(postComment.createdBy!!),
         content = postComment.content,
-        createdAt = postComment.createdAt.atOffset(ZONE_OFFSET),
-        updatedAt = postComment.updatedAt.atOffset(ZONE_OFFSET)
+        createdAt = postComment.createdAt,
+        updatedAt = postComment.updatedAt,
     )
 }
 
